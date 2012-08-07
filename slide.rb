14badc85c4df514507989396dce80d3ed9730b6d
buffpojken <daniel@sykewarrior.com>
2012-08-07 20:24:19 +0200

    ninja

--- slide.rb
+++ slide.rb
@@ -60,6 +60,15 @@ get '/stream' do
     page.html{ erb :stream }                   
     page.js{ "#{params[:callback]}(#{{:image => {:url => "http://kanan.vassaro.net" + @data.photo.url}}.to_json})" }
   end
+end           
+
+get '/fastest' do 
+  @data = Photo.order("ride_time").limit(1).first   
+  puts @data.inspect
+  respond_to do |page|
+    page.html{(@data.ride_time / 1000).to_s}
+    page.txt{(@data.ride_time / 1000).to_s}
+  end
 end
 
 get '/about' do 
